170fa3ed9ff4c31c1a5d6df0c6c9d1710c448273
takatso-wits <569869@students.wits.ac.za>
2020-09-27 02:07:08 +0200

    Geolocate the General Practitioners.

--- app/src/main/java/co/micronano/gradhack/LocateGP.java
+++ app/src/main/java/co/micronano/gradhack/LocateGP.java
@@ -3,11 +3,24 @@ package co.micronano.gradhack;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
@@ -17,15 +30,34 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.utils.BitmapUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class LocateGP extends AppCompatActivity implements
-        OnMapReadyCallback, PermissionsListener {
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
+public class LocateGP extends AppCompatActivity implements
+        OnMapReadyCallback, MapView.OnDidFinishRenderingMapListener {
+
+
+    private static final String ICON_ID = "red-pin-icon-id";
+    private static final float DEFAULT_DESIRED_ICON_OFFSET = -16;
+    private static final float STARTING_DROP_HEIGHT = -100;
+    private static final long DROP_SPEED_MILLISECONDS = 1200;
+    private static final String SYMBOL_LAYER_ID = "symbol-layer-id";
     private MapView mapView;
-    private PermissionsManager permissionsManager;
-    private MapboxMap mapboxMap;
+    private SymbolLayer pinSymbolLayer;
+    private Style style;
+    private TimeInterpolator currentSelectedTimeInterpolator = new BounceInterpolator();
+    private ValueAnimator animator;
+    private boolean firstRunThrough = true;
+    private boolean animationHasStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,50 +69,158 @@ public class LocateGP extends AppCompatActivity implements
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
-
-    }
-
-    @SuppressWarnings( {"MissingPermission"})
-    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
-        // Check if permissions are enabled and if not request
-        if (PermissionsManager.areLocationPermissionsGranted(this)) {
-
-            // Get an instance of the component
-            LocationComponent locationComponent = mapboxMap.getLocationComponent();
-
-            // Activate with options
-            locationComponent.activateLocationComponent(
-                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
-
-            // Enable to make component visible
-            locationComponent.setLocationComponentEnabled(true);
-
-            // Set the component's camera mode
-            locationComponent.setCameraMode(CameraMode.TRACKING);
-
-            // Set the component's render mode
-            locationComponent.setRenderMode(RenderMode.COMPASS);
-        } else {
-            permissionsManager = new PermissionsManager(this);
-            permissionsManager.requestLocationPermissions(this);
-        }
     }
 
 
     @Override
-    @SuppressWarnings({"Missing Permissions"})
+    public void onDidFinishRenderingMap(boolean fully) {
+        initAnimation(currentSelectedTimeInterpolator);
+        initInterpolatorButtons();
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        mapboxMap.setStyle(new Style.Builder().fromUri(Style.LIGHT)
+// Add GeoJsonSource with random Features to the map.
+                        .withSource(new GeoJsonSource("source-id",
+                                FeatureCollection.fromFeatures(new Feature[] {
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                119.86083984375,
+                                                -1.834403324493515)),
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                116.06637239456177,
+                                                5.970619502704659)),
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                114.58740234375,
+                                                4.54357027937176)),
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                118.19091796875,
+                                                5.134714634014467)),
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                110.36865234374999,
+                                                1.4500404973608074)),
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                109.40185546874999,
+                                                0.3076157096439005)),
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                115.79589843749999,
+                                                1.5159363834516861)),
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                113.291015625,
+                                                -0.9667509997666298)),
+                                        Feature.fromGeometry(Point.fromLngLat(
+                                                116.40083312988281,
+                                                -0.3392008994314591))
+                                })
+                        ))
+                        .withImage(ICON_ID, BitmapUtils.getBitmapFromDrawable(
+                                getResources().getDrawable(R.drawable.mapbox_marker_icon_default))), new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        LocateGP.this.style = style;
+                        mapView.addOnDidFinishRenderingMapListener(LocateGP.this);
+                    }
+                }
+        );
+
+    }
+    private void initAnimation(TimeInterpolator desiredTimeInterpolator) {
+        if (animator != null) {
+            animator.cancel();
+        }
+        animator = ValueAnimator.ofFloat(STARTING_DROP_HEIGHT, -17);
+        animator.setDuration(DROP_SPEED_MILLISECONDS);
+        animator.setInterpolator(desiredTimeInterpolator);
+        animator.setStartDelay(1000);
+        animator.start();
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                if (!animationHasStarted) {
+                    initSymbolLayer();
+                    animationHasStarted = true;
+                }
+                pinSymbolLayer.setProperties(iconOffset(new Float[] {0f, (Float) valueAnimator.getAnimatedValue()}));
+            }
+        });
+    }
+
+    private void initSymbolLayer() {
+        pinSymbolLayer = new SymbolLayer(SYMBOL_LAYER_ID,
+                "source-id");
+        pinSymbolLayer.setProperties(
+                iconImage(ICON_ID),
+                iconIgnorePlacement(true),
+                iconAllowOverlap(true),
+                iconOffset(new Float[] {0f, DEFAULT_DESIRED_ICON_OFFSET}));
+        style.addLayer(pinSymbolLayer);
+    }
+
+    private void initInterpolatorButtons() {
+
+        FloatingActionButton bounceInterpolatorFab = findViewById(R.id.fab_bounce_interpolator);
+        bounceInterpolatorFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentSelectedTimeInterpolator = new BounceInterpolator();
+                resetIcons();
+            }
+        });
+
+        FloatingActionButton linearInterpolatorFab = findViewById(R.id.fab_linear_interpolator);
+        linearInterpolatorFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentSelectedTimeInterpolator = new LinearInterpolator();
+                firstRunThrough = false;
+                resetIcons();
+            }
+        });
+
+        FloatingActionButton accelerateInterpolatorFab = findViewById(R.id.fab_accelerate_interpolator);
+        accelerateInterpolatorFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentSelectedTimeInterpolator = new AccelerateInterpolator();
+                firstRunThrough = false;
+                resetIcons();
+            }
+        });
+
+        FloatingActionButton decelerateInterpolatorFab = findViewById(R.id.fab_decelerate_interpolator);
+        decelerateInterpolatorFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentSelectedTimeInterpolator = new DecelerateInterpolator();
+                firstRunThrough = false;
+                resetIcons();
+            }
+        });
+    }
+
+    private void resetIcons() {
+        if (!firstRunThrough) {
+            animationHasStarted = false;
+            style.removeLayer(SYMBOL_LAYER_ID);
+            initAnimation(currentSelectedTimeInterpolator);
+        }
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         mapView.onStart();
     }
+
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         mapView.onResume();
     }
 
     @Override
-    protected void onPause() {
+    public void onPause() {
         super.onPause();
         mapView.onPause();
     }
@@ -88,6 +228,9 @@ public class LocateGP extends AppCompatActivity implements
     @Override
     protected void onStop() {
         super.onStop();
+        if (animator != null) {
+            animator.end();
+        }
         mapView.onStop();
     }
 
@@ -109,44 +252,5 @@ public class LocateGP extends AppCompatActivity implements
         mapView.onDestroy();
     }
 
-    @Override
-    public void onExplanationNeeded(List<String> permissionsToExplain) {
-        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
-
-    }
-
-    @Override
-    public void onPermissionResult(boolean granted) {
-        if (granted) {
-            mapboxMap.getStyle(new Style.OnStyleLoaded() {
-                @Override
-                public void onStyleLoaded(@NonNull Style style) {
-                    enableLocationComponent(style);
-                }
-            });
-        } else {
-            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
-            finish();
-        }
-
-    }
-
-    @Override
-    public void onMapReady(@NonNull MapboxMap mapboxMap) {
-        LocateGP.this.mapboxMap = mapboxMap;
-
-        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/cjerxnqt3cgvp2rmyuxbeqme7"),
-                new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        enableLocationComponent(style);
-                    }
-                });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
 
 }
